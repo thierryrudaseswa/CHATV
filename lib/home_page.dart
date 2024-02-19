488d21bb37716c3c751e66addaac486436f4cc08
thierryrudaseswa <trudaseswa@gmail.com>
2024-02-19 20:14:07 +0200

    The VOICE CHAT app that helps in dictating the words build in flutter

---
+++ lib/home_page.dart
@@ -0,0 +1,79 @@
+import 'package:chatvoice/pallete.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Thierry"),
+        leading: const Icon(Icons.menu),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+         Stack(
+  children: [
+    Container(
+      height: 120,
+      width: 120,
+      margin: const EdgeInsets.only(top: 4),
+      decoration: BoxDecoration(
+        color: Pallete.assistantCircleColor,
+        shape: BoxShape.circle,
+      ),
+    ),
+    Positioned(
+      top: 4,
+      left: 4,
+      child: Container(
+        height: 112,
+        width: 112,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(
+            image: AssetImage('assets/images/virtualAssistant.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    ),
+  ],
+),
+
+
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(top: 30),
+            decoration: BoxDecoration(
+              border: Border.all(color: Pallete.borderColor,
+              )
+            ),
+
+            child:const  Padding(
+              padding:  EdgeInsets.all(8.0),
+              child:  Text(
+                "Good Morning , what task  can I do for yout?",
+                style: TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.mainFontColor,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+import 'package:chatvoice/pallete.dart';
+import 'package:chatvoice/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Allen',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Pallete.whiteColor,
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
